0a3faff682fe5ac40597abcc458bc3cd0c57563a
Andre Cabalo <andreccabalo@gmail.com>
2024-07-26 11:54:00 -0300

    Reforçando práticas java

---
+++ src/Soma.java
@@ -0,0 +1,10 @@
+public class Soma {
+    public static void main(String [] args) {
+        int a = 10;
+        int b = 5;
+        int soma = a + b;
+        System.out.println("A soma de " + a + " e " + b + " é " + soma);
+        System.out.printf("A soma de " + a + " e " + b + " é " + soma);
+        System.out.println("A soma de " + a + " e " + b + " é " + soma);
+    }
+}
---
+++ src/Subtracao.java
@@ -0,0 +1,10 @@
+public class Subtracao {
+    public static void main(String [] args) {
+        int a = 10;
+        int b = 5;
+        int subtracao = a - b;
+        System.out.println("A subtração de " + a + " e " + b + " é " + subtracao);
+        System.out.printf("A subtração de " + a + " e " + b + " é " + subtracao);
+        System.out.println("A subtração de " + a + " e " + b + " é " + subtracao);
+    }
+}
